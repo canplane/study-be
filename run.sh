1ea53595693249a939419fc5d4b20b514acc7b0d
canplane <canplane@gmail.com>
2023-10-24 13:44:13 +0900

    requirement 1 resolved

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp target/classes:target/dependency/* webserver/WebServer ${1:-8080} &
+java -cp target/classes:target/dependency/* webserver/WebServer ${1:-8080}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +20,44 @@ public class RequestHandler extends Thread {
         this.connection = connectionSocket;
     }
 
+    private String[] getRequestHeader(InputStream in) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        List<String> _lines = new ArrayList<>();
+        String line;
+        while (!"".equals(line = reader.readLine()) && line != null) {
+            _lines.add(line);
+        }
+        String[] lines = _lines.toArray(new String[0]);
+        return lines;
+    }
+    private String getUrl(String[] requestHeader) {
+        String requestHeaderFirstLine = requestHeader[0];
+        String url = requestHeaderFirstLine.split(" ")[1];
+        return url;
+    }
+    private byte[] getFileByUrl(String url) throws IOException {
+        if (url.equals("/")) {
+            url = "/index.html";
+        }
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        return body;
+    }
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
+            /** 요구사항 1 */
+            String[] requestHeader = getRequestHeader(in);
+            String url = getUrl(requestHeader);
+            byte[] body = getFileByUrl(url);
+
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World, 상훈!".getBytes();
+            //byte[] body = "Hello World, 상훈!".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
